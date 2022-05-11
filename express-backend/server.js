08ea6387679b4875d35c911f0ebe0a34e454e009
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2022-05-11 20:37:04 +0700

    Add seedData, Model & Routes Products

--- express-backend/server.js
+++ express-backend/server.js
@@ -3,6 +3,9 @@ import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
+
 dotenv.config();
 
 mongoose
@@ -15,27 +18,8 @@ mongoose
   });
 
 const app = express();
-
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
-app.get('/api/product/slug/:slug', (req, res) => {
-  const product = data.products.find((p) => p.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product not found' });
-  }
-});
-
-app.get('/api/product/:id', (req, res) => {
-  const product = data.products.find((p) => p._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product not found' });
-  }
-});
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () =>
